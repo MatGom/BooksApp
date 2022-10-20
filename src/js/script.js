b19c97a22fce5b9109a3f5afbfc7946b1b1b9965
Mateusz Gomolka <mdm.gomolka@gmail.com>
2022-10-20 21:28:49 +0100

    Add function initActions

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@
 
   const booksList = document.querySelector('.books-list');
   const booksTemplate = Handlebars.compile(document.querySelector('#template-book').innerHTML);
-  console.log(booksTemplate);
+  console.log(dataSource);
 
   const render = function () {
     for (const book of dataSource.books) {
@@ -16,5 +16,22 @@
     }
   };
 
+  const favoriteBooks = [];
+
+  const initActions = function () {
+    const bookImages = document.querySelectorAll('.book__image');
+    console.log(bookImages);
+
+    for (let i = 0; i < bookImages.length; i++) {
+      bookImages[i].addEventListener('dblclick', function (e) {
+        e.preventDefault();
+        this.classList.add('favorite');
+        favoriteBooks.push(this.getAttribute('data-id'));
+      });
+    }
+    console.log(favoriteBooks);
+  };
+
   render();
+  initActions();
 }
