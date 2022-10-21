eef5d6c962e633be68abd0a53eb925f145c60fd0
Mateusz Gomolka <mdm.gomolka@gmail.com>
2022-10-21 19:15:53 +0100

    Remove favorite and id

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,35 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 {
   ('use strict');
 
   const booksList = document.querySelector('.books-list');
   const booksTemplate = Handlebars.compile(document.querySelector('#template-book').innerHTML);
-  console.log(dataSource);
 
   const render = function () {
     for (const book of dataSource.books) {
-      /* generate HTML based on template */
       const generatedHTML = booksTemplate(book);
-      /* create element using utils.createElementFromHTML */
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
-      /* add element to menu */
       booksList.appendChild(generatedDOM);
     }
   };
 
-  const favoriteBooks = [];
+  let favoriteBooks = [];
 
   const initActions = function () {
     const bookImages = document.querySelectorAll('.book__image');
-    console.log(bookImages);
 
     for (let i = 0; i < bookImages.length; i++) {
       bookImages[i].addEventListener('dblclick', function (e) {
         e.preventDefault();
-        this.classList.add('favorite');
-        favoriteBooks.push(this.getAttribute('data-id'));
+        if (!bookImages[i].classList.contains('favorite')) {
+          this.classList.add('favorite');
+          favoriteBooks.push(this.getAttribute('data-id'));
+        } else {
+          this.classList.remove('favorite');
+          favoriteBooks = favoriteBooks.filter(item => item !== this.getAttribute('data-id'));
+        }
       });
     }
-    console.log(favoriteBooks);
   };
 
   render();
